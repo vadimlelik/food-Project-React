4d167867abeb92ab3f63ab628bc5b2c537f8adc8
vadimcvirko <promiti90@gmail.com>
2021-06-29 14:58:33 +0300

    базовый роутинг

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import NotFound from './pages//NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="container content">
+      <Router>
+        <Switch>
+        <Route exact path='/' component={Home} />
+          <Route  path='/сontact' component={Contact} />
+          <Route  path='/about' component={About} />
+          <Route component={NotFound}/>
+        </Switch>
+        </Router>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+
+function Header() {
+    return (
+        <nav className='green darken-1'>
+            <div className='nav-wrapper'>
+                <a href='/' className='brand-logo'>
+                    React Shop
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a
+                            href='https://github.com/vadimlelik/food-project'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Repo
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export { Header };
