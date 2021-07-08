2d968fdd4b33c88d8d25170cec6ce02791fa42ff
vadimcvirko <promiti@yandex.ru>
2021-07-08 21:14:41 +0300

    feat: add search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Search = () => {
-  return <div></div>;
+const Search = ({ cb = Function.prototype }) => {
+  const [value, setValue] = useState('')
+  const handleKey = (e) => {
+    if (e.key === 'Enter') {
+      handleSabmit()
+    }
+
+
+  }
+  const handleSabmit = () => {
+    cb(value);
+  }
+  return <div className='row'>
+    <div className="input-field col s12">
+      <input type="search"
+        id='search-field'
+        placeholder='search '
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handleKey}
+        value={value} />
+            <button className='btn'
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0
+
+      }}
+      onClick={handleSabmit}>Search</button>
+  </div>
+    </div>
+
 };
 
 export default Search;
