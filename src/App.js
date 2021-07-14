177e2f0d4a1325004ac2107ebb30a4a229ec2213
vadimcvirko <promiti90@gmail.com>
2021-07-14 09:21:13 +0300

    finish project

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ import NotFound from './pages//NotFound';
 import Category from './pages/Category';
 import Recipe from './pages/Recipe';
 
-
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/food-project">
         <Header />
         <main className="container content">
           <Switch>
@@ -21,7 +20,7 @@ function App() {
             <Route path="/сontact" component={Contact} />
             <Route path="/about" component={About} />
             <Route path="/category/:name" component={Category} />
-            <Route path="/meal/:id" component={Recipe}/>
+            <Route path="/meal/:id" component={Recipe} />
             <Route component={NotFound} />
           </Switch>
         </main>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Search = () => {
-  return <div></div>;
+const Search = ({ cd = Function.prototype }) => {
+  const [value, setValue] = useState('');
+  const handleKey = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+  const handleSubmit = () => {};
+  return (
+    <div className="row">
+      <div className="input-field col s12">
+        <input
+          type="search"
+          id="search-field"
+          placeholder="search"
+          onKeyDown={handleKey}
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+      </div>
+      <button
+        className="btn "
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+        }}
+        onClick={handleSubmit}>
+        Search{' '}
+      </button>
+    </div>
+  );
 };
 
 export default Search;
